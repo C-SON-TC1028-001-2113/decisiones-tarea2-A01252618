5012eacbfbb40a2431f7e424ca683e644f7ae001
A01252618 <A01252618@itesm.mx>
2021-08-24 01:59:14 +0000

    ejercicio licencia

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -3,25 +3,19 @@ def main():
     edad = int(input("Ingresa tu edad: ")) 
     a = edad >= 18
     b = edad > 0
-
-    if a and b:
-        id = str(input("¿Tienes identificación oficial? (s/n): "))
-        if id == "s" : 
+    if a and b :
+        ine = str(input("¿Tienes identificación oficial? (s/n): "))
+        if ine == "s": 
             print ("Trámite de licencia concedido")
-
-        elif id == "n": 
+        elif ine == "n": 
             print ("No cumples requisitos")
-
-        else: 
-            print ("Respuesta incorrecta")
-        
+        else : 
+            print ("Respuesta incorrrecta")   
     if not a and b:
             print ("No cumples requisitos")
     if not a and not b: 
             print ("No cumples requisitos")
         
-
-
 if __name__=='__main__':
     main()
 
